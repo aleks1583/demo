addf94c21ca6f50e32ad6053b7f7c689e53626f8
aleks1583 <aleksejbanmin@gmail.com>
2026-05-30 10:19:08 +0300

    Add files via upload
    сделал скрипт для слайдера и интегрировал его на главную страницу.

--- index.php
+++ index.php
@@ -5,24 +5,24 @@ session_start();
 <html lang="ru">
 <head>
   <meta charset="UTF-8">
-  <title>Пассажирам.РФ</title>
+  <title>Пассажиры.РФ</title>
   <link rel="stylesheet" href="styles/style.css">
   <link rel="stylesheet" href="styles/slider.css">
 </head>
 <body>
-<!-- Шапка сайта -->
+
 <div class="header">
   <div class="nav">
-    <a href="index.php" class="logo">Пассажирам.РФ</a>
-    <!-- Кнопки для неавторизованных -->
+    <a href="index.php" class="logo">Пассажиры.РФ</a>
+    
     <?php if(!isset($_SESSION['user_id'])): ?>
       <div class="nav-buttons">
         <a href="login.php" class="btn-login">Войти</a>
         <a href="register.php" class="btn-register">Регистрация</a>
       </div>
-    <!-- Кнопки для администратора -->
+  
     <?php elseif($_SESSION['admin']): ?>
-      <!-- Обработка кнопки выхода -->
+    
       <?php if(isset($_GET['index'])) { 
         session_destroy(); 
         header('Location:index.php'); 
@@ -31,7 +31,7 @@ session_start();
         <a href="admin.php" class="btn-admin">Панель администратора</a>
         <a href="?index=1" class="btn-exit">Выход</a>
       </div>
-    <!-- Кнопки для обычных пользователей -->
+   
     <?php else: ?>
       <div class="nav-buttons">
         <a href="history.php" class="btn-lk">Мои заявки</a>
@@ -41,6 +41,33 @@ session_start();
   </div>
 </div>
 
+<div class="slideshow-container">
+  <div class="mySlides fade">
+    <img src="foto 1.png" style="width:100%">
+    <div class="text">Слайд 1</div>
+  </div>
+  <div class="mySlides fade">
+    <img src="foto 2.jpg" style="width:100%">
+    <div class="text">Слайд 2</div>
+  </div>
+  <div class="mySlides fade">
+    <img src="foto 3.jpg" style="width:100%">
+    <div class="text">Слайд 3</div>
+  </div>
+  <div class="mySlides fade">
+    <img src="foto 4.jpg" style="width:100%">
+    <div class="text">Слайд 4</div>
+  </div>
+  
+  <a class="prev" onclick="plusSlides(-1)">❮</a>
+  <a class="next" onclick="plusSlides(1)">❯</a>
+</div>
+<div class="dot-container">
+  <span class="dot active" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+  <span class="dot" onclick="currentSlide(4)"></span>
+</div>
 <script src='script/script.js'></script>
 </body>
 </html>
